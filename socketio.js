c330de7119639cd42b115322e1f51dc6779cd424
bernardo nunes <bmn02@outlook.com>
2024-10-18 18:42:02 -0300

    feat: add some features to chat project

---
+++ socketio.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("usuario conectado " + socket.id);
+  socket.on("message", (msg) => {
+    console.log(msg);
+    io.emit("message", msg);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(3000);
