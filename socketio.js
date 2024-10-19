58fa0e0c825b1499f66a53d85712c9dd4cbb9191
Guilherme emilio <guilhermeemilio4@gmail.com>
2024-10-19 14:41:21 -0300

    commit

--- socketio.js
+++ socketio.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
-
 const app = express();
 const server = http.createServer(app);
-
 const io = socketio(server);
-
 io.on("connection", (socket) => {
   console.log("usuario conectado " + socket.id);
   socket.on("message", (msg) => {
@@ -14,9 +11,7 @@ io.on("connection", (socket) => {
     io.emit("message", msg);
   });
 });
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
 server.listen(3000);
