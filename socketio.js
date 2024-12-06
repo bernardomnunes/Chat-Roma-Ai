6c150c06ab59f1eba826a2d0770985b7e6e6aed4
bernardo <bmn02@outlook.com>
2024-12-06 17:10:11 -0300

    feat: add socketio to api gemini

--- socketio.js
+++ socketio.js
@@ -4,10 +4,24 @@ const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const axios = require("axios");
+
 io.on("connection", (socket) => {
   console.log("usuario conectado " + socket.id);
-  socket.on("message", (msg) => {
-    console.log(msg);
+  socket.on("message", async (msg) => {
+    try {
+      // Ajustando a URL e o método HTTP
+      const response = await axios({
+        method: "get",
+        url: "http://localhost:3001/openai/chat",
+        data: {
+          prompt: msg,
+        },
+      });
+      console.log(response.data.prompt);
+    } catch (error) {
+      console.error("Erro ao enviar mensagem:", error);
+    }
     io.emit("message", msg);
   });
 });
